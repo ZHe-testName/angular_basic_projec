1dec511b06c9f3bbed70922755f7816509ee1971
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2022-04-13 22:44:00 +0300

    Added validation for my controls

--- basic/src/app/form/forms/forms.component.ts
+++ basic/src/app/form/forms/forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -13,13 +13,37 @@ export class FormsComponent implements OnInit {
   //так как формы реактивны то вся работа с ними будет из сомпонента а не шаблона
   ngOnInit(): void {
     //инитим форму
+    //констуктор принимает объект с конролами
     this.form = new FormGroup({
+      //каждый конрол это екземпляр соответствующего класса
+      //первый параметр конструктора 
+        //стартовое знаачение
+      //второй
+        //валидаторы
+        //может быть один
+        //можеет быть массив ч набором
 
+        //присутствует стандартный клас валидаторов
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
     });
   }
 
   submit() {
-    console.log('Form submitted>>>', this.form);
+    //также защитить отправку не валидной формы можно в методe submit
+    if(this.form.invalid) {
+      console.log('Form submitted>>>', this.form);
+
+      const formData = {...this.form.value};
+
+      console.log('form data', formData);
+    };
   };
 
 }
